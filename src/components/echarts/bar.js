1059e035eb93710530bf8f298cd6aeda1c002e9c
saz <xushaung@digits8.com>
2019-08-14 18:15:50 +0800

    bar

--- src/components/echarts/bar.js
+++ src/components/echarts/bar.js
@@ -9,28 +9,39 @@ import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
 
 class EchartsTest extends Component {
-   
+    constructor(props){
+        super(props)
+        this.state={
+            idount:'echart'+Math.random().toString().substring(2,9),
+        }
+    }
+    echartsVal={
+        myChart:null
+    }
     componentDidMount() {
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('main'));
-        // 绘制图表
-        myChart.setOption({
-            title: { text: 'ECharts 入门示例' },
-            tooltip: {},
-            xAxis: {
-                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        });
+        this.ininBar()
+    }
+    ininBar=()=>{
+           
+           // 基于准备好的dom，初始化echarts实例
+           this.echartsVal.myChart=echarts.init(document.getElementById(this.state.idount))
+          // 绘制图表
+          this.echartsVal.myChart.showLoading({
+              backgroundColor:'#000'
+          })
+          if( this.echartsVal.myChart!=null){
+              this.echartsVal.myChart.clear()
+              this.echartsVal.myChart.setOption(this.props.getOption);
+              setTimeout(()=>{
+                this.echartsVal.myChart.hideLoading()
+
+              },1000)
+
+          }
     }
     render() {
         return (
-            <div id="main" style={{ width: 400, height: 400 }}></div>
+            <div id={this.state.idount} style={{ width: 400, height: 400 }}></div>
         );
     }
 }
--- src/views/echarts/index.js
+++ src/views/echarts/index.js
@@ -1,53 +1,44 @@
 import React from 'react';
-import {Card} from 'antd';
+import { Card } from 'antd';
 // import echartTheme from './../themeLight'
 //不是按需加载的话文件太大
 //import echarts from 'echarts'
 //下面是按需加载
 import echarts from 'echarts/lib/echarts'
 //导入折线图
-import 'echarts/lib/chart/line';  //折线图是line,饼图改为pie,柱形图改为bar
-import 'echarts/lib/component/tooltip';
-import 'echarts/lib/component/title';
-import 'echarts/lib/component/legend';
-import 'echarts/lib/component/markPoint';
+// import 'echarts/lib/chart/line';  //折线图是line,饼图改为pie,柱形图改为bar
+// import 'echarts/lib/component/tooltip';
+// import 'echarts/lib/component/title';
+// import 'echarts/lib/component/legend';
+// import 'echarts/lib/component/markPoint';
 import EchartsTest from '../../components/echarts/bar'
-export default class Line extends React.Component{
-  componentWillMount(){
+export default class Line extends React.Component {
+  componentWillMount() {
     //主题的设置要在willmounted中设置
-    echarts.registerTheme('Imooc','red');
+    echarts.registerTheme('Imooc', 'red');
   }
-  getOption =()=> {
+  getOption = () => {
     let option = {
-      title:{
-        text:'用户骑行订单',
-        x:'center'
+      title: { text: 'ECharts 入门示例' },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
       },
-      tooltip:{
-        trigger:'axis',
-      },
-      xAxis:{
-        data:['周一','周二','周三','周四','周五','周六','周日']
-      },
-      yAxis:{
-        type:'value'
-      },
-      series:[
-        {
-          name:'OFO订单量',
-          type:'line',   //这块要定义type类型，柱形图是bar,饼图是pie
-          data:[1000,2000,1500,3000,2000,1200,800]
-        }
-      ]
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
     }
-   return option
+    return option
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <Card title="折线图表之一">
-            <EchartsTest/>
+          <EchartsTest getOption={this.getOption()} />
         </Card>
 
       </div>
