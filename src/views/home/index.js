39606845be83a6935695251ad60d27ae0c7cb3fe
saz <xushaung@digits8.com>
2019-08-27 20:36:10 +0800

    g

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import {Card} from 'antd'
+import {Card,Button} from 'antd'
 import Filter from '@/components/home/homeCustm/filter'
 export default class Home extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            headoff:false
+        }
+    }
+    parentsFnHome=(arr)=>{
+  console.log(arr)
+    }
     render(){
         return(
             <div>
                 {/* 删选 */}
-                <Filter/>
+                <div className="home_main_top">
+                    <Button style={{position:'absolute',right: 0,zIndex:32}} onClick={()=>{this.setState({headoff:!this.state.headoff})}}>自定义</Button>
+                    {this.state.headoff?<Filter parentsFnHome={this.parentsFnHome}/>:''}
+                </div>
                 <Card>
 
                 </Card>
